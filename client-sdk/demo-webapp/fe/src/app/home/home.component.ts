449543e1ff7043f8f859b3c26eea79d1f115814b
Alex Tavor <alex.tavor@gmail.com>
2025-12-19 15:41:13 +0100

    Integrate generated types

--- client-sdk/demo-webapp/fe/src/app/home/home.component.ts
+++ client-sdk/demo-webapp/fe/src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { JsonPipe } from '@angular/common';
+import { DiamondUser } from '@diamond/sdk';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatCardModule, JsonPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  sampleUser: DiamondUser = {
+    id: '12345',
+    name: 'Christoph',
+    email: 'christoph@diamond.dev',
+    role: 'admin',
+    createdAt: new Date(),
+  };
+}
